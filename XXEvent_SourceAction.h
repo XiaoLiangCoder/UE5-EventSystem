7f3b57e5725bd821f291400b844df5ab52401453
XiaoLiangCoder <53684116+XiaoLiangCoder@users.noreply.github.com>
2024-07-05 22:05:39 +0800

    Update XXEvent_SourceAction.h

--- XXEvent_SourceAction.h
+++ XXEvent_SourceAction.h
@@ -13,10 +13,6 @@ template<typename SourceType, typename ActionType>
 struct XUANXUE_API FXXEvent_SourceAction
 {
 public:
-	FXXEvent_SourceAction( SourceType&& _InSource, ActionType&& _InAction )
-		:m_Source( _InSource ), m_Action( _InAction ) {};
-
-public:
 	/// Source : 事件发起者的标识
 	SourceType m_Source;
 
@@ -27,13 +23,7 @@ public:
 
 template<typename SourceType, typename ActionType, typename ResultType>
 struct XUANXUE_API FXXEvent_SourceActionWithResult : public FXXEvent_SourceAction<SourceType, ActionType>
-{
-public:
-	/// 万能引用(&&) + 完美转发(std::forward)
-	FXXEvent_SourceActionWithResult( SourceType&& _InSource, ActionType&& _InAction, ResultType&& _InResult )
-		:FXXEvent_SourceAction<SourceType, ActionType>( 
-			std::forward<SourceType>( _InSource ), std::forward<ActionType>( _InAction ) ), m_Result{ _InResult }{};
-	
+{	
 public:
 	/// Result: Source执行Action产生的结果
 	ResultType m_Result;
